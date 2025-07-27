db63a87a37fa26701104e965eebb38544d59d575
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2025-07-28 01:02:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $filesJson = json_encode($files);
   <meta charset="UTF-8" />
   <title>Fail2Ban Report</title>
   <link rel="stylesheet" href="/assets/css/style.css" />
-  <script src="/assets/js/jsonreader.js"></script>
+  <script src="/assets/js/jsonreader.js" defer></script>
 </head>
 <body>
   <h1>Fail2Ban Report</h1>
