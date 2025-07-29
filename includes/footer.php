639dca5320e4c980da537b03ec6061959ec6b0a4
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2025-07-29 16:52:25 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer class="site-footer">
   <div class="footer-content">
-    &copy; <?php echo date('Y'); ?> Your Company Name. All rights reserved.
+    &copy; <?php echo date('Y'); ?> SubleXBle (<a href="https://github.com/SubleXBle" title="SubleXBle on Github" target="_blank">https://github.com/SubleXBle</a>) All rights reserved.
   </div>
 </footer>
