2637b8966abe109f7e4d13920bdcc98bea15dc59
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2025-07-30 18:35:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,6 @@
 <?php include ('includes/list-files.php'); ?>
 <?php // index.php Version 0.2.1 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <title>Fail2Ban Report</title>
-  <link rel="stylesheet" href="assets/css/style.css" />
-  <script>
-    const availableFiles = <?php echo $filesJson; ?>;
-  </script>
-  <script src="assets/js/jsonreader.js" defer></script>
-  <script src="assets/js/action-collector.js" defer></script>
-  <script src="assets/js/action.js" defer></script>
-  <script src="assets/js/blocklist-overlay.js" defer></script>
-</head>
-<body>
-  
- <div class="inline-headlines"> 
-  <h1>Fail2Ban-Report</h1>
-  <h2>Let's catch the bad guys!</h2>
- </div>
+<?php include 'includes/header.php'; ?>
   
   <label for="dateSelect">Select Date:</label>
   <select id="dateSelect"></select>
