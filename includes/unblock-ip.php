b7b9d76b38a7d9534f731c861d49dc6655955ca3
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2025-08-07 21:22:47 +0200

    Update unblock-ip.php
    will now set pending = true on unblocking action

--- includes/unblock-ip.php
+++ includes/unblock-ip.php
@@ -58,6 +58,7 @@ function unblockIp($ip, $jail) {
             }
 
             $item['active'] = false;
+            $item['pending'] = true;
             $item['lastModified'] = date('c');
             $found = true;
             break;
