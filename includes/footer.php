c18791e18a558420a359505bec9872bdc6e325ca
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2025-08-16 20:39:40 +0200

    Delete includes directory

--- includes/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="site-footer">
-  <div class="footer-content">
-    <span title="Made with sweat, tears and love by SubleXBle">Made with 🥵, 😿 and ❤️ </span>by <a href="https://github.com/SubleXBle" title="SubleXBle on Github" target="_blank" class="yellowtext">SubleXBle</a> &copy; <?php echo date('Y'); ?> All rights reserved
-  </div>
-</footer>
