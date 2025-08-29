62bfa0435f06c6bbea2e65b2f05ec0edd35992c6
SubleXBle <51033045+SubleXBle@users.noreply.github.com>
2025-08-29 22:32:50 +0200

    Update header.php

--- Web-UI/includes/header.php
+++ Web-UI/includes/header.php
@@ -8,7 +8,7 @@
   <title>Fail2Ban Report</title>
   <meta name="viewport" content="width=device-width, initial-scale=0.8">
   <link rel="stylesheet" href="assets/css/style.css" />
-  <link rel="icon" href="assets/images/favicon-1.png" type="image/png">
+  <link rel="icon" href="assets/css/favicon-32x32.png" type="image/png">
   <script>
     const availableFiles = <?php echo $filesJson; ?>;
   </script>
